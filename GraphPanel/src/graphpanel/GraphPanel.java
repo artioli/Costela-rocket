1f54b89d23d080fc72011da39a3dace3d5680e12
Artioli <victorartioli@gmail.com>
2016-10-05 15:50:09 -0300

    projeto java

---
+++ GraphPanel/src/graphpanel/GraphPanel.java
@@ -0,0 +1,288 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphpanel;
+
+import java.awt.BasicStroke; //The BasicStroke class defines a basic set of rendering attributes for the outlines of graphics primitives, which are rendered with a Graphics2D object that has its Stroke attribute set to this BasicStroke.
+import java.awt.BorderLayout; //A border layout lays out a container, arranging and resizing its components to fit in five regions: north, south, east, west, and center.
+import java.awt.Color; //The Color class is used to encapsulate colors in the default sRGB color space or colors in arbitrary color spaces identified by a ColorSpace.
+import java.awt.Dimension; // The Dimension class encapsulates the width and height of a component (in integer precision) in a single object.
+import java.awt.FontMetrics; // The FontMetrics class defines a font metrics object, which encapsulates information about the rendering of a particular font on a particular screen.
+import java.awt.Graphics; //The Graphics class is the abstract base class for all graphics contexts that allow an application to draw onto components that are realized on various devices, as well as onto off-screen images
+import java.awt.Graphics2D; //This Graphics2D class extends the Graphics class to provide more sophisticated control over geometry, coordinate transformations, color management, and text layout.
+import java.awt.Point; // A point representing a location in (x,y) coordinate space, specified in integer precision.
+import java.awt.RenderingHints; // The RenderingHints class defines and manages collections of keys and associated values which allow an application to provide input into the choice of algorithms used by other classes which perform rendering and image manipulation services.
+import java.awt.Stroke; // The Stroke interface allows a Graphics2D object to obtain a Shape that is the decorated outline, or stylistic representation of the outline, of the specified Shape.
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+/**
+ *
+ * @author vartioli
+ */
+
+public class GraphPanel extends JPanel {
+
+    private final int width = 800;
+    private final int heigth = 400;
+    private final int padding = 25;
+    private final int labelPadding = 25;
+    private final Color lineColor = new Color(44, 102, 230, 180);
+    private final Color pointColor = new Color(100, 100, 100, 180);
+    private final Color gridColor = new Color(200, 200, 200, 200);
+    private static final Stroke GRAPH_STROKE = new BasicStroke(2f);
+    private final int pointWidth = 3;
+    private final int numberYDivisions = 10;
+    private List<Double> scores;
+
+    public GraphPanel(List<Double> scores) {
+        this.scores = scores;
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        double xScale = ((double) getWidth() - (2 * padding) - labelPadding) / (scores.size() - 1);
+        double yScale = ((double) getHeight() - 2 * padding - labelPadding) / (getMaxScore() - getMinScore());
+
+        List<Point> graphPoints = new ArrayList<>();
+        for (int i = 0; i < scores.size(); i++) {
+            int x1 = (int) (i * xScale + padding + labelPadding);
+            int y1 = (int) ((getMaxScore() - scores.get(i)) * yScale + padding);
+            graphPoints.add(new Point(x1, y1));
+        }
+
+        // draw white background
+        g2.setColor(Color.WHITE);
+        g2.fillRect(padding + labelPadding, padding, getWidth() - (2 * padding) - labelPadding,
+                getHeight() - 2 * padding - labelPadding);
+        g2.setColor(Color.BLACK);
+
+        // create hatch marks and grid lines for y axis.
+        for (int i = 0; i < numberYDivisions + 1; i++) {
+            int x0 = padding + labelPadding;
+            int x1 = pointWidth + padding + labelPadding;
+            int y0 = getHeight()
+                    - ((i * (getHeight() - padding * 2 - labelPadding)) / numberYDivisions + padding + labelPadding);
+            int y1 = y0;
+            if (scores.size() > 0) {
+                g2.setColor(gridColor);
+                g2.drawLine(padding + labelPadding + 1 + pointWidth, y0, getWidth() - padding, y1);
+                g2.setColor(Color.BLACK);
+                String yLabel = ((int) ((getMinScore()
+                        + (getMaxScore() - getMinScore()) * ((i * 1.0) / numberYDivisions)) * 100)) / 100.0 + "";
+                FontMetrics metrics = g2.getFontMetrics();
+                int labelWidth = metrics.stringWidth(yLabel);
+                g2.drawString(yLabel, x0 - labelWidth - 5, y0 + (metrics.getHeight() / 2) - 3);
+            }
+            g2.drawLine(x0, y0, x1, y1);
+        }
+
+        // and for x axis
+        for (int i = 0; i < scores.size(); i++) {
+            if (scores.size() > 1) {
+                int x0 = i * (getWidth() - padding * 2 - labelPadding) / (scores.size() - 1) + padding + labelPadding;
+                int x1 = x0;
+                int y0 = getHeight() - padding - labelPadding;
+                int y1 = y0 - pointWidth;
+                if ((i % ((int) ((scores.size() / 20.0)) + 1)) == 0) {
+                    g2.setColor(gridColor);
+                    g2.drawLine(x0, getHeight() - padding - labelPadding - 1 - pointWidth, x1, padding);
+                    g2.setColor(Color.BLACK);
+                    String xLabel = i + "";
+                    FontMetrics metrics = g2.getFontMetrics();
+                    int labelWidth = metrics.stringWidth(xLabel);
+                    g2.drawString(xLabel, x0 - labelWidth / 2, y0 + metrics.getHeight() + 3);
+                }
+                g2.drawLine(x0, y0, x1, y1);
+            }
+        }
+
+        // create x and y axes
+        g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding, padding + labelPadding, padding);
+        g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding, getWidth() - padding,
+                getHeight() - padding - labelPadding);
+
+        Stroke oldStroke = g2.getStroke();
+        g2.setColor(lineColor);
+        g2.setStroke(GRAPH_STROKE);
+        for (int i = 0; i < graphPoints.size() - 1; i++) {
+            int x1 = graphPoints.get(i).x;
+            int y1 = graphPoints.get(i).y;
+            int x2 = graphPoints.get(i + 1).x;
+            int y2 = graphPoints.get(i + 1).y;
+            g2.drawLine(x1, y1, x2, y2);
+        }
+
+        g2.setStroke(oldStroke);
+        g2.setColor(pointColor);
+        for (int i = 0; i < graphPoints.size(); i++) {
+            int x = graphPoints.get(i).x - pointWidth / 2;
+            int y = graphPoints.get(i).y - pointWidth / 2;
+            int ovalW = pointWidth;
+            int ovalH = pointWidth;
+            g2.fillOval(x, y, ovalW, ovalH);
+        }
+    }
+
+    // @Override
+    // public Dimension getPreferredSize() {
+    // return new Dimension(width, heigth);
+    // }
+    private double getMinScore() {
+        double minScore = Double.MAX_VALUE;
+        for (Double score : scores) {
+            minScore = Math.min(minScore, score);
+        }
+        return minScore;
+    }
+
+    private double getMaxScore() {
+        double maxScore = Double.MIN_VALUE;
+        for (Double score : scores) {
+            maxScore = Math.max(maxScore, score);
+        }
+        return maxScore;
+    }
+
+    public void setScores(List<Double> scores) {
+        this.scores = scores;
+        invalidate();
+        this.repaint();
+    }
+
+    public List<Double> getScores() {
+        return scores;
+    }
+
+    private static void createAndShowGui() {
+        List<Double> scores = new ArrayList<>();
+        Random random = new Random();
+        int maxDataPoints = 120;
+        int maxScore = 3500;
+        for (int i = 0; i < maxDataPoints; i++) {
+            scores.add((double) random.nextDouble() * maxScore);
+            // scores.add((double) i);
+        }
+        MainPanel mainPanel = new MainPanel(scores);
+        mainPanel.setPreferredSize(new Dimension(800, 600));
+        JFrame frame = new JFrame("DrawGraph");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(mainPanel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            createAndShowGui();
+        });
+    }
+
+    //Labels
+
+    static class MainPanel extends JPanel {
+
+        public MainPanel(List<Double> scores) {
+
+            setLayout(new BorderLayout());
+
+            JLabel title = new JLabel("Comparação da altura com tempo percorrido pelo foguete");
+            title.setFont(new Font("Arial", Font.BOLD, 25));
+            title.setHorizontalAlignment(JLabel.CENTER);
+
+            JPanel graphPanel = new GraphPanel(scores);
+
+            VerticalPanel vertPanel = new VerticalPanel();
+
+            HorizontalPanel horiPanel = new HorizontalPanel();
+
+            add(title, BorderLayout.NORTH);
+            add(horiPanel, BorderLayout.SOUTH);
+            add(vertPanel, BorderLayout.WEST);
+            add(graphPanel, BorderLayout.CENTER);
+
+        }
+
+        class VerticalPanel extends JPanel {
+
+            public VerticalPanel() {
+                setPreferredSize(new Dimension(25, 0));
+            }
+
+            @Override
+            public void paintComponent(Graphics g) {
+
+                super.paintComponent(g);
+
+                Graphics2D gg = (Graphics2D) g;
+                gg.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+                Font font = new Font("Arial", Font.PLAIN, 15);
+
+                String string = "Altura (m)";
+
+                FontMetrics metrics = g.getFontMetrics(font);
+                int width = metrics.stringWidth(string);
+                int height = metrics.getHeight();
+
+                gg.setFont(font);
+
+                drawRotate(gg, getWidth(), (getHeight() + width) / 2, 270, string);
+            }
+
+            public void drawRotate(Graphics2D gg, double x, double y, int angle, String text) {
+                gg.translate((float) x, (float) y);
+                gg.rotate(Math.toRadians(angle));
+                gg.drawString(text, 0, 0);
+                gg.rotate(-Math.toRadians(angle));
+                gg.translate(-(float) x, -(float) y);
+            }
+
+        }
+
+        class HorizontalPanel extends JPanel {
+
+            public HorizontalPanel() {
+                setPreferredSize(new Dimension(0, 25));
+            }
+
+            @Override
+            public void paintComponent(Graphics g) {
+
+                super.paintComponent(g);
+
+                Graphics2D gg = (Graphics2D) g;
+                gg.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+                Font font = new Font("Arial", Font.PLAIN, 15);
+
+                String string = "Tempo (s)";
+
+                FontMetrics metrics = g.getFontMetrics(font);
+                int width = metrics.stringWidth(string);
+                int height = metrics.getHeight();
+
+                gg.setFont(font);
+
+                gg.drawString(string, (getWidth() - width) / 2, 11);
+            }
+
+        }
+
+    }
+
+}
